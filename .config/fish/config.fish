c445b1967a8614c615b05d8c65a76a2f53cee663
syl20bnr <sylvain.benner@gmail.com>
2014-09-18 20:50:53 -0400

    Add JAVA_HOME

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -47,6 +47,15 @@ end
 # force english language
 set -x LC_ALL en_GB.UTF-8
 
+# java home
+# /usr/libexec/java_home allows to select easily the version
+# of java installed on the system
+# to force a specific version:
+# /usr/libexec/java_home -v 1.6
+# to list the available versions:
+# /usr/libexec/java_home -V
+set -x JAVA_HOME (/usr/libexec/java_home)
+
 # ----------------------------------------------------------------------------
 # aliases
 # ----------------------------------------------------------------------------
